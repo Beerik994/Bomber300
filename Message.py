d7e05a8dd23773b9b0f57acdc697582235ed7763
Beerik994 <129565754+Beerik994@users.noreply.github.com>
2023-12-26 00:34:21 -0600

    update Message.py

--- Message.py
+++ Message.py
@@ -1,61 +1,59 @@
-
 message = '''
         [\033[32m1\033[33m] Bomber300
-        [\033[32m2\033[33m] Пробив номера телефона
-        [\033[32m3\033[33m] Настройки
-        [\033[32m4\033[33m] Телефонная книга
+        [\033[32m2\033[33m] Phone Number Lookup
+        [\033[32m3\033[33m] Settings
+        [\033[32m4\033[33m] Phonebook
         [\033[32m5\033[33m] MailBomber300
-        [\033[32m6\033[33m] Инструкция
-        [\033[31m0\033[33m] Выход 
+        [\033[32m6\033[33m] Instructions
+        [\033[31m0\033[33m] Exit 
 '''
 
-message1 = "\n        [\033[32m!\033[33m] Изменения применены !"
+message1 = "\n        [\033[32m!\033[33m] Changes applied!"
 message8 = """
                   \n
-                  \n                 Создатель идеи/Разработчик:
-                  \n        [\033[32mИван Зайцев\033[33m] - \033[32mhttps://vk.com/ivan_vzlom300\033[33m
-                  \n                \033[33mНад визуальными эффектами\Анимации работали:\033[33m
-                  \n        [\033[32mДима Чернышов\033[33m] - \033[32mhttps://vk.com/psih.odinochka\033[33m
-                  \n                \033[33mРаботали с сервисами:\033[33m
-                  \n        [\033[32mКирилл Гурьев\033[33m] - \033[32mhttps://vk.com/id560704224\033[33m
+                  \n                 Idea/Developer:
+                  \n        [\033[32mIvan Zaitsev\033[33m] - \033[32mhttps://vk.com/ivan_vzlom300\033[33m
+                  \n                \033[33mVisual Effects/Animations:
+                  \n        [\033[32mDima Chernyshov\033[33m] - \033[32mhttps://vk.com/psih.odinochka\033[33m
+                  \n                \033[33mServices:
+                  \n        [\033[32mKirill Guryev\033[33m] - \033[32mhttps://vk.com/id560704224\033[33m
                   \n
  """
 
 message10 = """
 
-        [\033[32m1\033[33m] Добавить номер
-        [\033[32m2\033[33m] Удалить номер
-        [\033[32m3\033[33m] Просмотр списка
-        [\033[32m0\033[33m] Выйти
+        [\033[32m1\033[33m] Add Number
+        [\033[32m2\033[33m] Delete Number
+        [\033[32m3\033[33m] View List
+        [\033[32m0\033[33m] Exit
 
 """
 
-message15 = '\n        [\033[31m!\033[33m] Проверка обновлений, пожалуйста подождите [\033[31m!\033[33m] '
+message15 = '\n        [\033[31m!\033[33m] Checking for updates, please wait [\033[31m!\033[33m] '
 message20 = '''
-        [\033[32m1\033[33m] Атака сообщениями
-        [\033[32m2\033[33m] Атака звонками
-        [\033[32m3\033[33m] Атака сообщения/звонки
-        [\033[31m0\033[33m] Выход 
+        [\033[32m1\033[33m] Message Attack
+        [\033[32m2\033[33m] Call Attack
+        [\033[32m3\033[33m] Message/Call Attack
+        [\033[31m0\033[33m] Exit 
 '''
 
 message_instruct = '''
-              [\033[31m!\033[33m] ИНСТРУКЦИЯ ПО ИСПОЛЬЗОВАНИЮ BOMBER300 [\033[31m!\033[33m]
+              [\033[31m!\033[33m] BOMBER300 USAGE INSTRUCTIONS [\033[31m!\033[33m]
 
 
         [\033[32mMAIL BOMBER\033[33m]
-        Для использования этой утилиты прочтите инструкцию. 
-        При использовании утилиты у вас могут возникнуть - 
-        вопросы по серверу для отправки писем, 
-        все сервера для своих почт написаны ниже. 
-        Перед этим программа попросит ввести данные почты с которой будет идти атака.
-        Советую для этого дела создать отдельную почту!
+        To use this utility, please read the instructions. 
+        When using the utility, you may have questions about the server for sending emails, 
+        all servers for different mail services are listed below. 
+        Before that, the program will ask you to enter the email credentials from which the attack will be sent.
+        I recommend creating a separate email for this purpose!
         
-        Google = [\033[32msmtp.gmail.com\033[33m]
+        Google = [\033[.gmail.com\033[33m]
         Yandex = [\033[32msmtp.yandex.ru\033[33m]
         Mail = [\033[32msmtp.mail.ru\033[33m]
 
 
         [\033[32mSMS BOMBER\033[33m]
-        В моей программе поддерживаются только Российские номера !
+        Only Russian phone numbers are supported in my program!
 
 '''
